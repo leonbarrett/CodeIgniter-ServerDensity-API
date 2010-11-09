b144a93ea5dbab0ab38a0e198d08a31ee33292d5
Leon Barrett <leon@leonbarrett.com>
2010-11-09 09:12:10 +0000

    Initial import

---
+++ config/server_density_config.php
@@ -0,0 +1,18 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+// ------------------------------------------------------------------------
+// ServerDensity API config
+// ------------------------------------------------------------------------
+
+//Server Density Account Name
+$config['sd_account_name'] = '';
+
+
+//Server Density User Name
+$config['sd_user_name'] = '';
+
+//Server Density Password
+$config['sd_password'] = '';
+
+
+?>
